42daa67d64313b14ea6c5aa36d0b76455f4788e6
Johny W. Alves <johnywalves@gmail.com>
2021-04-15 09:35:54 -0300

    :label: Adicionar tipagem para Cartas

---
+++ src/types/CardProps.ts
@@ -0,0 +1,32 @@
+export enum CardType {
+  Artifact = 0,
+  Creature = 1,
+  Spell = 2
+}
+
+export enum CardState {
+  Drawing = 0,
+  Hand = 1,
+  Spotlight = 2,
+  Attacking = 3,
+  Defensing = 4,
+  Deploy = 5,
+  Discard = 6
+}
+
+export enum CardReach {
+  Meelee = 0,
+  Distance = 1
+}
+
+type CardProps = {
+  type: CardType
+  level: number
+  name: string
+  description: string
+  attack?: number
+  defense?: number
+  reach?: CardReach
+}
+
+export default CardProps
