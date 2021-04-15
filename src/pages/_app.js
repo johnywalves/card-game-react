4863bfb676274ca00a7298016f6b6f14ce570cbf
Johny W. Alves <johnywalves@gmail.com>
2021-04-15 09:34:40 -0300

    :lipstick: Adição de estilo global

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import GlobalStyles from '../styles/global'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
